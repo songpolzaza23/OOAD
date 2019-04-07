7e56a71e4d9df2eb1b05e038ec3bc5a17f4e7434
-PLUEM- <59161123@go.buu.ac.th>
2019-04-07 15:22:04 +0700

    test

--- public/js/addUser/manageStudent.js
+++ public/js/addUser/manageStudent.js
@@ -107,4 +107,28 @@ function getStudent() {
       console.log("ERROR: ", e);
     }
   });
+
+}
+
+function updateStudents(){
+
+  $.ajax({
+      type: "POST",
+      contentType: "application/json",
+      url: "http://localhost:8000/updateStudents",
+      dataType: "json",
+      success: function (customer) {
+        // var result = JSON.stringify(customer);
+        console.log(customer);
+        if (customer != "") {
+          alert("Update Successful!");
+         
+        } else {
+          alert("Update Incorrect!");
+        }
+      },
+      error: function (e) {
+        console.log("ERROR: ", e);
+      }
+    });
 }
