1b1d03834a99efee502eacb693b62b011a1311ad
teerawatgithub <59160069@go.buu.ac.th>
2019-04-08 17:33:18 +0700

    server update for teacher

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var addStudent = require('./router/chooseManageUser')
 var getStudent = require('./router/manageStudent')
 var getStaff = require('./router/manageStaff')
 var manageTerm = require('./router/manageTerm')
+var manageTeacher = require('./router/manageTeacher')
 var path = require('path');
 
 app.use(express.static('public'));
@@ -20,7 +21,7 @@ app.use(login)
 app.use(addStudent)
 app.use(getStudent)
 app.use(manageTerm)
-
+app.use(manageTeacher)
 
 app.use(getStaff)
 app.listen(process.env.PORT || 8000, () => {
