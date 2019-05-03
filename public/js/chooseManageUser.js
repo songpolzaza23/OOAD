de942efe9ee7ed72dd939dd376639343001336cf
teerawatgithub <59160069@go.buu.ac.th>
2019-05-03 14:05:28 +0700

    ui not compress v2

--- public/js/chooseManageUser.js
+++ public/js/chooseManageUser.js
@@ -347,3 +347,59 @@ function checkExamer() {
     }
   });
 }
+
+function checkExamScore() {
+  var data = {
+    username: username,
+    type: type
+  }
+  console.log(data)
+  $.ajax({
+    type: "POST",
+    contentType: "application/json",
+    url: "http://localhost:8000/main/login",
+    data: JSON.stringify(data),
+    dataType: "json",
+    success: function (customer) {
+      // var result = JSON.stringify(customer);
+      console.log(customer);
+      if (customer != "") {
+        alert("Login Successful!");
+        window.location = "./../../main/checkExamScore/checkExamScore.html?" + customer[0].username + " " + customer[0].type;
+      } else {
+        alert("Login Incorrect!");
+      }
+    },
+    error: function (e) {
+      console.log("ERROR: ", e);
+    }
+  });
+}
+
+function viewExamList() {
+  var data = {
+    username: username,
+    type: type
+  }
+  console.log(data)
+  $.ajax({
+    type: "POST",
+    contentType: "application/json",
+    url: "http://localhost:8000/main/login",
+    data: JSON.stringify(data),
+    dataType: "json",
+    success: function (customer) {
+      // var result = JSON.stringify(customer);
+      console.log(customer);
+      if (customer != "") {
+        alert("Login Successful!");
+        window.location = "./../../main/viewExamList/viewExamList.html?" + customer[0].username + " " + customer[0].type;
+      } else {
+        alert("Login Incorrect!");
+      }
+    },
+    error: function (e) {
+      console.log("ERROR: ", e);
+    }
+  });
+}
